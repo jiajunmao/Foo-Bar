84e09505bb4f08b113940318b661479a63be135a
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-07-10 19:49:35 -0400

    Working level 2.5. Edge case adapted.

--- Level-2-5/src/Main.java
+++ Level-2-5/src/Main.java
@@ -1,32 +1,32 @@
 public class Main {
     public static int[] answer(int[] l, int t){
-
-        return null;
+        return consecutiveForward(l ,t);
     }
 
-    public static int[] consecutiveForward(int[] list, int startPos, int target){
+    public static int[] consecutiveForward(int[] list, int target){
         for(int pos=0; pos<list.length; pos++) {
             // Loop for increasing length at current position
-            for (int j = 1; j < list.length; j++) {
+            for (int j = 1; j < list.length-pos+1; j++) {
                 int segmentSum = 0;
 
-                for (int i = startPos; i < startPos + j; i++) {
+                for (int i = pos; i < pos + j; i++) {
                     segmentSum += list[i];
                 }
 
-                if (segmentSum == target)
+                if (segmentSum == target) {
                     // Start position and the length for the segment
-                    return new int[]{startPos, j};
+                    return new int[]{pos, pos+j-1};
+                }
             }
         }
         return new int[]{-1,-1};
     }
 
     public static void main(String args[]){
-        int[] a = new int[]{4,3,10,2,8};
+        int[] a = new int[]{4, 3, 5, 7, 8};
         int t = 12;
 
-        int[] temp = consecutiveForward(a, 0, t);
+        int[] temp = consecutiveForward(a, t);
         for(int i=0; i<temp.length; i++){
             System.out.print(temp[i] + ",");
         }
